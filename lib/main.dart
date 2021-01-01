fcf9ac1ccf2e62c2b8a0611a558b0349e0eeb678
Chinenye Raphael <cudraphael@gmail.com>
2021-01-01 23:58:42 +0100

    Implementing widgets - Day 2 #100DOC

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    Container(
-      color: Colors.blueAccent,
-    ),
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("The Rapixar App")
+        ),
+        body: myWidget(),
+      ),
+    );
+  }
+}
+
+Widget myWidget() {
+  return Padding(
+    padding: EdgeInsets.all(16.0),
+    child: Text("Hello, World!",),
   );
+
 }
